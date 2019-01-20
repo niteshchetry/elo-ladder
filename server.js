65d97aed6742d8fb69611915a39942fd45434aaf
niteshchetry <chetrynitesh@gmail.com>
2019-01-20 14:49:46 -0800

    testing for heroku

--- server.js
+++ server.js
@@ -1,24 +1,26 @@
 const express = require('express');
 const app = express();
-const db = require('./db');
+//const db = require('./db');
 const updateLeaderboard = require('./updateLeaderboard.js');
 
 app.set('views', __dirname + '/views')
 app.set('view engine', 'pug');
 
 app.get('/', function(req, res) {
-    
+    res.json([{name: "Devlove", elo: 1000, wins: 2, losses: 0}, 
+        {name: "Fjhkim", elo: 1000, wins: 0, losses: 2}]);
+    /*
     db.query("SELECT * FROM player ORDER BY elo DESC", function(err, result){
         if (err) throw err;
         res.json(result);
-        /*res.render('index', {ladder: result});
+        res.render('index', {ladder: result});
             const winner = req.query.winner;
             const loser = req.query.loser;
             if(winner != null && loser != null) {
                 updateLeaderboard(winner, loser);
-            }
-            */
+            }    
     });
+    */
 });
 
 app.listen(3003, () => {
